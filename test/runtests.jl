561b76efd3c5a6e70a3a8bb2961ebbaed4d81650
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-05-15 20:06:56 -0300

    Cleanup test dependencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,19 +6,7 @@ using CoDa
 using Unitful
 using LinearAlgebra
 using Statistics
-using Plots; gr(size=(600,400))
-using MeshPlots # TODO: replace by MeshViz
-using ReferenceTests, ImageIO
 using Test, Random
-
-# workaround GR warnings
-ENV["GKSwstype"] = "100"
-
-# environment settings
-isCI = "CI" ∈ keys(ENV)
-islinux = Sys.islinux()
-visualtests = !isCI || (isCI && islinux)
-datadir = joinpath(@__DIR__,"data")
 
 # list of tests
 testfiles = [
